89e9c05fe84c2aa1ddf5d15f41df31afce89193c
chutki12 <priyankachaudhary2068>
2022-09-01 19:24:36 +0530

    ADDED : Used Lombok For Logging Levels And Files And Patterns Are Handled In application.properties

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -1,15 +1,22 @@
 package com.bridgelabz.employeepayrollapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Use Lombok Library for Logging i.e Slf4j
+ */
+
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollAppApplication {
 
     public static void main(String[] args) {
 
         System.out.println("Welcome To Employee Payroll Application!");
         SpringApplication.run(EmployeePayrollAppApplication.class, args);
+        log.info("Employee Payroll App Started");
     }
 
 }
