5f8515260ae32e6e1791f8913191b220439c3418
chutki12 <priyankachaudhary2068>
2022-09-01 22:19:46 +0530

    ADDED : Created application.properties For Dev And Production

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -3,6 +3,7 @@ package com.bridgelabz.employeepayrollapp;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 /**
  * Use Lombok Library for Logging i.e Slf4j
@@ -16,7 +17,11 @@ public class EmployeePayrollAppApplication {
 
         System.out.println("Welcome To Employee Payroll Application!");
         SpringApplication.run(EmployeePayrollAppApplication.class, args);
-        log.info("Employee Payroll App Started");
+        ApplicationContext context = SpringApplication.run(EmployeePayrollAppApplication.class, args);
+        SpringApplication.run(EmployeePayrollAppApplication.class, args);
+        log.info("Employee Payroll App Started in {} Environment",
+                context.getEnvironment().getProperty("environment"));
     }
-
 }
+
+
